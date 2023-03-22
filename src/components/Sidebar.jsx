06ab1df28a995e7cd1e93ff218c262832d490feb
Manoel Lima <manoelsslima@yahoo.com.br>
2023-03-22 02:28:52 -0400

    Applying boder to sidebar avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
             />
             <div className={styles.profile}>
-                <Avatar src="https://github.com/manoelsslima.png" />
+                <Avatar hasBorder={true} src="https://github.com/manoelsslima.png" />
                 <strong>Manoel Lima</strong>
                 <span>Web Developer</span>
             </div>
